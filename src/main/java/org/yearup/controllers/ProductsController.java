b95cf6e69f3b3890bf96a1b919c5de4cb969502a
Joshua <Joshua32149@gmail.com>
2024-01-02 10:52:14 -0500

    fixed bugs

--- src/main/java/org/yearup/controllers/ProductsController.java
+++ src/main/java/org/yearup/controllers/ProductsController.java
@@ -81,7 +81,7 @@ public class ProductsController
     {
         try
         {
-            productDao.create(product);
+            productDao.update(id,product);
         }
         catch(Exception ex)
         {
